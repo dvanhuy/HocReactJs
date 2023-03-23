eb1f9f0237aae84be7f513eb16977c5500a50d5d
unknown <huydinhvan13@gmail.com>
2023-03-23 19:40:49 +0700

    s

--- src/useContextProvider/paragraph.js
+++ src/useContextProvider/paragraph.js
@@ -7,8 +7,7 @@ function Paragraph(){
         <>
             <div className={getTheme.theme}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
             Excepturi a officia explicabo, sit voluptates fugit quibusdam,
-             suscipit eligendi laborum voluptatem omnis. Ut voluptates consequatur 
-             eveniet quaerat impedit fuga eligendi expedita.</div>
+             suscipit eligendi laborum voluptatem omnis.</div>
         </>
     )
 }
